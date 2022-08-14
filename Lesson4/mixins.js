bf85456a09ad2a8c49c4c506ed5e6afcb6576678
Joshua Lindsay <99047296+JoshuaWLindsay@users.noreply.github.com>
2022-08-14 07:06:07 -0500

    mixins

--- Lesson4/mixins.js
+++ Lesson4/mixins.js
@@ -1,50 +1,100 @@
-const Moveable = {
-  range() {
-    return this.fuelCap *  this.fuelEfficiency;
-  }
-};
+// const Moveable = {
+//   range() {
+//     return this.fuelCap *  this.fuelEfficiency;
+//   }
+// };
 
-class WheeledVehicle {
-  constructor(tirePressure, kmTravelledPerLiter, fuelCapInLiter) {
-    this.tires = tirePressure;
-    this.fuelEfficiency = kmTravelledPerLiter;
-    this.fuelCap = fuelCapInLiter;
+// class WheeledVehicle {
+//   constructor(tirePressure, kmTravelledPerLiter, fuelCapInLiter) {
+//     this.tires = tirePressure;
+//     this.fuelEfficiency = kmTravelledPerLiter;
+//     this.fuelCap = fuelCapInLiter;
+//   }
+
+//   tirePressure(tireIdx) {
+//     return this.tires[tireIdx];
+//   }
+
+//   inflateTire(tireIdx, pressure) {
+//     this.tires[tireIdx] = pressure;
+//   }
+// }
+
+// Object.assign(WheeledVehicle.prototype, Moveable);
+
+// class Auto extends WheeledVehicle {
+//   constructor() {
+//     // the array represents tire pressure for four tires
+//     super([30,30,32,32], 50, 25.0);
+//   }
+// }
+
+// class Motorcycle extends WheeledVehicle {
+//   constructor() {
+//     // array represents tire pressure for two tires
+//     super([20,20], 80, 8.0);
+//   }
+// }
+
+// class Catamaran {
+//   constructor(propellerCount, hullCount, kmTravelledPerLiter, fuelCapInLiter) {
+//     // catamaran specific logic
+
+//     this.propellerCount = propellerCount;
+//     this.hullCount = hullCount;
+//     this.kmTravelledPerLiter = kmTravelledPerLiter;
+//     this.fuelCapInLiter = fuelCapInLiter;
+//   }
+// }
+
+// Object.assign(Catamaran.prototype, Moveable);
+
+class Chef {
+  prepare(wedding) {
+    this.prepareFood(wedding.guests);
   }
 
-  tirePressure(tireIdx) {
-    return this.tires[tireIdx];
-  }
-
-  inflateTire(tireIdx, pressure) {
-    this.tires[tireIdx] = pressure;
+  prepareFood(guests) {
+    console.log(guests);
   }
 }
 
-Object.assign(WheeledVehicle.prototype, Moveable);
+class Decorator {
+  prepare(wedding) {
+    this.decoratePlace(wedding.flowers);
+  }
 
-class Auto extends WheeledVehicle {
-  constructor() {
-    // the array represents tire pressure for four tires
-    super([30,30,32,32], 50, 25.0);
+  decoratePlace(flowers) {
+    console.log(flowers);
   }
 }
 
-class Motorcycle extends WheeledVehicle {
-  constructor() {
-    // array represents tire pressure for two tires
-    super([20,20], 80, 8.0);
+class Musician {
+  prepare(wedding) {
+    this.preparePerformance(wedding.songs);
+  }
+
+  preparePerformance(songs) {
+    console.log(songs);
   }
 }
 
-class Catamaran {
-  constructor(propellerCount, hullCount, kmTravelledPerLiter, fuelCapInLiter) {
-    // catamaran specific logic
+class Wedding {
+  constructor(guests, flowers, songs) {
+    this.guests = guests;
+    this.flowers = flowers;
+    this.songs = songs;
+  }
 
-    this.propellerCount = propellerCount;
-    this.hullCount = hullCount;
-    this.kmTravelledPerLiter = kmTravelledPerLiter;
-    this.fuelCapInLiter = fuelCapInLiter;
+  prepare(preparers) {
+    preparers.forEach(preparer => {
+      preparer.prepare(this);
+    });
   }
 }
 
-Object.assign(Catamaran.prototype, Moveable);
+let lindsayWedding = new Wedding(['mom', 'dad'], ['roses'], ['My Girl']);
+
+let supportStaff = [new Chef(), new Decorator(), new Musician()];
+
+lindsayWedding.prepare(supportStaff);
